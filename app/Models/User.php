36ed19f33a6129c87a7a89de01a2683ae22d9f66
Authentic48 <iboufall48@gmail.com>
2022-03-31 20:22:11 +0700

    Post model, table and controller created

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
     {
      return null !== $this->roles()->where('name', $role)->first();
     }
+
+    public function posts()
+    {
+       return $this->hasMany('App\Models\Post');
+    }
 }
